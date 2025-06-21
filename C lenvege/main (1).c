cfdda2bf718b78b654ea76f59d85fae2e7265ed0
krishflutter993 <savaliyakrish993@gmail.com>
2025-06-21 11:48:31 +0530

    Add files via upload

--- C lenvege/main (1).c
+++ C lenvege/main (1).c
@@ -1,30 +1,46 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
+#include <stdio.h>
+#include <conio.h>
+
+void main() 
 {
-    int age;
+    int choice;
+    float base, height, length, breadth, radius, area;
+
     
-    printf("enter your age:");
-    scanf("%d",&age);
-    
-    if(age>18)
-    { 
-        printf("\t eligible vote");
-        
-        if(age>60)
-        {
-            printf("\t your a under siniar citizan category");
-        }
-        else
-        {
-           printf("\t your a not  under siniar citizan category");
-        }
-        
-    }
-    
-    else
+    printf("1. Area of Triangle\n");
+    printf("2. Area of Rectangle\n");
+    printf("3. Area of Circle\n");
+    scanf("%d", &choice);
+
+    if (choice == 1) 
     {
-      printf("\t not eligible vote");
-    }    
+        printf("Enter base  the triangle: ");
+        scanf("%f", &base);
+        printf("Enter height  the triangle: ");
+        scanf("%f", &height);
+        area = 0.5 * base * height;
+        printf("\n Area of Triangle = %f", area);
+    }
+    else if (choice == 2) 
+    {
+        printf("Enter length of the rectangle: ");
+        scanf("%f", &length);
+        printf("Enter breadth of the rectangle: ");
+        scanf("%f", &breadth);
+        area = length * breadth;
+        printf(" \n Area of Rectangle = %f", area);
+    }
+    else if (choice == 3)
+    {
+        printf("Enter radius of the circle: ");
+        scanf("%f", &radius);
+        area = 3.14 * radius * radius;
+        printf("\n Area of Circle = %f", area);
+    }
+    else 
+    {
+        printf("your number is not valid");
+    }
+
     getch();
 }
