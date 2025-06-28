5cdbac82395dd05efdf6b17f59bd3ca7fd090b0d
krishflutter993 <savaliyakrish993@gmail.com>
2025-06-28 10:06:52 +0530

    Add files via upload

---
+++ C lenvege/28-6/main (2).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int cal()
+{
+    int a = 6;
+    int b = 5;
+    int c=a+b;
+   
+    return c;
+}
+
+int main()
+{
+    
+    printf("%d",cal());
+      
+       return 0;
+}
---
+++ C lenvege/28-6/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int cal(int a,int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    printf("\n %d",cal(6,5));
+    printf("\n %d",cal(5,5));
+    printf("\n %d",cal(4,5));
+    printf("\n %d",cal(3,5));
+    
+    
+    return 0;
+}
