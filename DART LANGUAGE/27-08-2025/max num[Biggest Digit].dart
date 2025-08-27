65e26ba4654aa1981b42994d4f02db2b050280f9
krishflutter993 <savaliyakrish993@gmail.com>
2025-08-27 17:20:36 +0530

    commit

---
+++ DART LANGUAGE/27-08-2025/max num[Biggest Digit].dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main()
+{
+  var max = 0;
+
+  print("Enter Any Number");
+  var num = int.parse(stdin.readLineSync().toString());
+
+  for (; num > 0; )
+  {
+    int rem = num % 10;
+    if (rem > max)
+    {
+      max = rem;
+    }
+    num = num ~/ 10;
+  }
+
+  print("Biggest digit is: $max");
+
+}
