661105cd912c5c991bfb987d2e39cd9772cec698
krishflutter993 <savaliyakrish993@gmail.com>
2025-11-18 11:04:57 +0530

    commit

---
+++ onlion db /18-11-2025/api/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2>Krish API</h2>";
+echo "<ul>";
+echo "<li><a href='connect.php'>Connection</a></li>";
+echo "<li><a href='signup.php'>Signup</a></li>";
+echo "<li><a href='view.php'>View</a></li>";
+echo "<li><a href='update.php'>Update</a></li>";
+echo "<li><a href='delete.php'>Delete</a></li>";
+echo "<li><a href='insert.php'>Insert</a></li>";
+echo "<li><a href='signin.php'>Signin</a></li>";
+echo "</ul>";
+?>
