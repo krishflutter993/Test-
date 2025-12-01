40c717d7d2f0be16d10a59f36205a01d69af6260
krishflutter993 <savaliyakrish993@gmail.com>
2025-12-01 10:01:04 +0530

    commit

--- onlion db /18-11-2025/api/insert.php
+++ onlion db /18-11-2025/api/insert.php
@@ -2,45 +2,42 @@
 
 include('connect.php');
 
-// Upload folder (inside api/)
+// Upload folder
 $upload_path = 'images/';
 
-// Localhost URL (must be http)
-$upload_url = 'http://' . $_SERVER['SERVER_NAME'] . "/api/" . $upload_path;
+// Generate upload URL
+$upload_url = 'https://' . $_SERVER['SERVER_NAME'] . "/api/" . $upload_path;
 
 // Request fields
 $p_name  = $_POST['p_name'];
 $p_price = $_POST['p_price'];
 $p_des   = $_POST['p_des'];
 
-// File info
-$fileinfo = pathinfo($_FILES["p_img"]["p_name"]);
+// File information
+$fileinfo = pathinfo($_FILES["p_img"]["name"]);
+
+// File extension
 $extension = $fileinfo["extension"];
+
+// Random file name
 $random = 'image_' . rand(1000, 9999);
 
-// File name
-$file_name = $random . '.' . $extension;
+// URL stored in database
+$file_url = $upload_url . $random . '.' . $extension;
 
-// Final save location
-$server_path = $upload_path . $file_name;
+// Server upload path
+$file_path = $upload_path . $random . '.' . $extension;
 
-// Final URL stored in DB
-$file_url = $upload_url . $file_name;
+// Upload file
+move_uploaded_file($_FILES["p_img"]["tmp_name"], $file_path);
 
-// Move file to images/
-if (move_uploaded_file($_FILES["p_img"]["tmp_name"], $server_path)) {
+// SQL INSERT
+$sql = "INSERT INTO krish_products(p_name, p_price, p_des, p_img) 
+        VALUES ('$p_name', '$p_price', '$p_des', '$file_url')";
 
-    // Insert into database
-    $sql = "INSERT INTO krish_products (p_name, p_price, p_des, p_img)
-            VALUES ('$p_name', '$p_price', '$p_des', '$file_url')";
-    
-    mysqli_query($con, $sql);
+mysqli_query($con, $sql);
 
-    echo "Uploaded Successfully";
-
-} else {
-    echo "Upload Failed";
-}
-
+// Close connection
 mysqli_close($con);
+
 ?>
